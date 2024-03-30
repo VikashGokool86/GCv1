52ac55a67b199ecf1cf9d3544afc1e4585dd122b
Vikash Gokool <vikashgokool@yahoo.com>
2024-03-30 04:16:26 +0200

    -  transferred styles to the other 2 courses

--- goldCirclev1/resources/js/components/LyonParilly.vue
+++ goldCirclev1/resources/js/components/LyonParilly.vue
@@ -6,13 +6,33 @@
 </script>
 
 <template>
-    <div class="home_wrapper">
-        <h1>Lyon-Parilly (FR)</h1>
-
+    <div class="">
         <div v-if="loading">Loading...</div>
-        <div v-else class="home_wrapper2">
-            {{ data }}
+        <div v-else class="">
 
+            <div class="cp_header">
+                <div class="cp_header_title">Lyon-Parilly (FR)</div>
+                <div class="cp_header_course">Course: Poly</div>
+                <div class="cp_header_date">
+                    {{ data.racecard.dayOfWeek }} - {{ data.racecard.day }} {{ data.racecard.month }} {{ data.racecard.year }}
+                </div>
+                <div class="cp_nav_wrapper">
+                    <div v-for="(raceItem, index) in races" :key="raceItem.race" @click="selectRaceItem(raceItem, raceItem.race)" >
+                        <div class="cp_nav_index" >
+                            <div :class="{ is_active: activeNumber === raceItem.race }" >  {{  raceItem.race }}</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div v-if="currentRaceItem">
+                    <RaceDetails :race="currentRaceItem" />
+                    </div>
+                <div v-else class="">
+                    <RaceDetails :race="races[0]" />
+                </div>
+            </div>
         </div>
 
     </div>
@@ -20,21 +40,41 @@
 
 <script>
 
+    import RaceDetails from './RaceDetails.vue';
+
     export default {
+        components: {
+            RaceDetails
+        },
         data() {
             return {
-                data: null
-            };
+                data: null,
+                loading: true,
+                races: [],
+                currentRaceItem: null,
+                activeNumber: 1
+            }
         },
         methods: {
             async fetchData() {
                 try {
                     const response = await fetch("/data/RaceCard_LYO.json");
                     this.data = await response.json();
+                    this.loading = false;
+                    this.races = this.data.racecard.races ? this.data.racecard.races : {};
+                    // gets the number of races
                 } catch (error) {
                     console.error('Error fetching data:', error);
                 }
+            },
+            selectRaceItem(item, number) {
+                this.setActive(number)
+                this.currentRaceItem = item;
+            },
+            setActive(number) {
+                this.activeNumber = number;
             }
+
         },
         mounted() {
             this.fetchData();
@@ -43,5 +83,44 @@
 </script>
 
 <style>
+    .cp_header{
+        text-align: center;
+        background: #000;
+        color: aliceblue;
+        padding: 4px;
+    }
+
+    .cp_header_title{
+        font-size: 2em;
+    }
+
+    .cp_header_course{
+        font-size: 1.2em;
+    }
+
+    .cp_header_date{
+        font-size: 1.2em;
+    }
+
+    .cp_nav_wrapper{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .cp_nav_index{
+        padding: 2px;
+        color: aliceblue;
+        border: 2px aliceblue solid;
+        border-radius: 2px;
+        padding: 6px;
+        margin: 4px;
+        font-weight: bold;
+    }
+
+.is_active {
+    background-color: #fff;
+    color: #000;
+}
 
 </style>
